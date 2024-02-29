5b2bf0c07a4509d62eb95638e71c0fe3bbef39a1
Roxana Vargas <roxvargasc@gmail.com>
2024-02-29 16:35:20 -0500

    Add evaluation with ragas

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect
+from aiohttp import request
+from flask import Flask, jsonify, send_from_directory, render_template, redirect
+import json
+from datasets import Dataset
+from ragas import evaluate
+from ragas.metrics import faithfulness, answer_relevancy, context_recall, context_precision
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -16,6 +22,73 @@ def home():
 @app.route('/<path:path>')
 def all_routes(path):
     return redirect('/')
+
+os.environ["OPENAI_API_KEY"] = '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'
+
+@app.route('/evaluate', methods=['GET'])
+def evaluate_answers():
+    try:
+        # data_request = request.json
+
+        # questions = data_request.get("questions", [])
+        # ground_truth = data_request.get("ground_truth", [])
+        # answers = data_request.get("answers", [])
+        # contexts = data_request.get("contexts", [])
+        questions = [
+                    "Qué dijo el presidente sobre la crisis política?", 
+                    ]
+        # Después
+        ground_truth = [
+        "El presidente dijo que la crisis política se debe a la corrupción y la inflación",
+        ]
+
+        answers = [
+            ["El presidente dijo que la crisis política es a causa de la corrupción"],
+        ]
+        # answers = []
+        # contexts = []
+        contexts = [
+            ["El presidente mencionó en su discurso que la crisis política que atravesamos se debe al crecimiento de la corrupción",]
+        ]
+
+        data = {
+            "question": questions,
+            "answer": [str(answer) for answer in answers],  # Convertir todos los valores a cadenas
+            "contexts": contexts,
+            "ground_truth": ground_truth
+        }
+        dataset = Dataset.from_dict(data)
+
+        result = evaluate(
+            dataset=dataset, 
+            metrics=[
+                context_precision,
+                context_recall,
+                faithfulness,
+                answer_relevancy,
+            ],
+            raise_exceptions=False
+        )
+
+        pd.set_option("display.max_colwidth", None)
+        df = result.to_pandas()
+
+        # Convertir el DataFrame a un diccionario
+        result_dict = df.to_dict(orient="records")
+
+        # Convertir cualquier ndarray a lista para que sea JSON serializable
+        result_dict_serializable = json.loads(json.dumps(result_dict, default=lambda x: x.tolist()))
+
+        return jsonify({
+            "statusCode": 200,
+            "body": result_dict_serializable,
+        })
+
+    except Exception as e:
+        return jsonify({
+            "statusCode": 500,
+            "body": f"Error during evaluation: {str(e)}",
+        })
 
 if __name__ == "__main__":
     app.run(port=port)
